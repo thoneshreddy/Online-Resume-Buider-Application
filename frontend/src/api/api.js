ec19c8941bd75c63904241fbbe79ca8883e21276
thonesh <bunny@gmail.com>
2026-02-01 16:15:44 +0530

    Resdy for vercel deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://resume-builder-backend-on39.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
   const token =
     localStorage.getItem("token") || sessionStorage.getItem("token");
 
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
   return req;
 });
 
